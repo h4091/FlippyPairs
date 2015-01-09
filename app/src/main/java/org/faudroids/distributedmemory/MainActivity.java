236dbcdb6c4dae83d38fe36df9b232b9cf89dd71
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-01-09 16:55:22 +0100

    Added simple host / join UI

--- app/src/main/java/org/faudroids/distributedmemory/MainActivity.java
+++ app/src/main/java/org/faudroids/distributedmemory/MainActivity.java
@@ -2,14 +2,32 @@ package org.faudroids.distributedmemory;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+		findViewById(R.id.game_host).setOnClickListener(this);
+		findViewById(R.id.game_join).setOnClickListener(this);
     }
 
+
+	@Override
+	public void onClick(View view) {
+		switch(view.getId()) {
+			case R.id.game_host:
+				Toast.makeText(this, "Hosting game", Toast.LENGTH_SHORT).show();
+				break;
+
+			case R.id.game_join:
+				Toast.makeText(this, "Joining game", Toast.LENGTH_SHORT).show();
+				break;
+		}
+	}
 }
